dfba33623de265f97c04e1dd7074b1622dd2dce6
Samin Yeasar <samin.cse.uiu@gmail.com>
2021-08-17 23:56:50 +0600

    prettier

--- Basic Algorithm Scripting/02_reverseString.js
+++ Basic Algorithm Scripting/02_reverseString.js
@@ -1,28 +1,27 @@
-function reverseString(str){
-    //Reversing string using built-in functions
-    //=============================================
-    return str.split("").reverse().join("");
-    /**
+function reverseString(str) {
+  //Reversing string using built-in functions
+  //=============================================
+  return str.split("").reverse().join("");
+  /**
    * Reversing string with recursion
    */
   //   if (str === "") return str;
   //   else return reverseString(str.substr(1) + str.charAt(0));
 }
-console.log(reverseString('apple'));
+console.log(reverseString("apple"));
 
 //Reversing string using decrementing for loop
 //=============================================
-  function reverseString2(str){
-    let newString = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-      newString += str[i];
-    }
-
-    return newString;
+function reverseString2(str) {
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
   }
-console.log(reverseString2('apple'));
 
+  return newString;
+}
+console.log(reverseString2("apple"));
 
 let str = "Hello world!";
-console.log(str.substr(6,3));//wor
-console.log(str.charAt(6));//w 
+console.log(str.substr(6, 3)); //wor
+console.log(str.charAt(6)); //w
